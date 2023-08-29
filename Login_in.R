1a493224eca92ebd93809efe41c615387ac26400
IdanHajbeko <92939120+IdanHajbeko@users.noreply.github.com>
2023-08-30 00:08:39 +0300

    Add files via upload

--- Login_in.R
+++ Login_in.R
@@ -30,9 +30,9 @@ start <- function(style = 1){
   }
   
   cat("\033[32mWelcome to the login page created in R\033[0m\n")
-  cat("Created by idan hajbeko/n")
-  cat("This is like hack the box just it bad i made it and it in R")
-  cat("use the commands in that order to start -help, -rules, -story, -start")
+  cat("\033[32mCreated by idan hajbeko\033[0m\n")
+  cat("\033[32mThis is like hack the box just it bad i made it and it in R\033[0m\n")
+  cat("\033[32mUse the commands in that order to start help, levels, rules, story, start\033[0m\n")
 }
 
 help <- function(){
@@ -40,32 +40,62 @@ help <- function(){
 
 The help command provides information about the available commands and their usage. Here is a list of commands and a brief description of each:
 
-1. **Command Name 1**
-   - Description: Brief description of what this command does.
-   - Usage: How to use the command, including any required arguments or options.
-   - Example: An example of how the command is used in a real scenario.
+1. **rules**
+   - \033[32mDescription\033[0mdsf: Disply the rules of the lab what to do and what not.
+   - \033[34mUsage\033[0m: To use the command just type: \"rules\" in the terminal.
+   - \033[31mExample\033[0m: rules.
 
-2. **Command Name 2**
-   - Description: Brief description of what this command does.
-   - Usage: How to use the command, including any required arguments or options.
-   - Example: An example of how the command is used in a real scenario.
+2. **story**
+   - \033[32mDescription\033[0m: Tell you the story of the .
+   - \033[34mUsage\033[0m: Type: \"story level_number\" in the terminal.
+   - \033[31mExample\033[0m: story 1\033[0m.
 
-3. **Command Name 3**
-   - Description: Brief description of what this command does.
-   - Usage: How to use the command, including any required arguments or options.
-   - Example: An example of how the command is used in a real scenario.
+3. **levels**
+   - \033[32mDescription\033[0m: Show you all the levels.
+   - \033[34mUsage\033[0m: Just type \"levels\" in the terminal.
+   - \033[31mExample\033[0m: levels.
 
-...
-")
+4. **start**
+   - \033[32mDescription\033[0m: Start the lab with the level you choose.
+   - \033[34mUsage\033[0m: Type: \"start level_number\".
+   - \033[31mExample\033[0m: start 3.
+  
+5. **quit**
+   - \033[32mDescription\033[0m: Quit the programm.
+   - \033[34mUsage\033[0m: Just type: \"quit\" in the terminal.
+   - \033[31mExample\033[0m: quit.")
+}
+
+rules <- function(){
+  cat("__rules will be here__\n")
 }
 
 terminal <- function(){
+  while (TRUE){
+    command = readline("terminal>  ")
+    if (command == "help"){
+      help()
+    }
+    
+    else if (command == "rules"){
+      rules()
+    }
   
+    else if (command == "quit"){
+      break
+    }
+    
+    else if (command == ""){
+      
+    }
+    
+    else{
+      cat(paste0("\033[31m", "No command named ", '"' , command , '"', " check it and try agin", "\033[0m\n"))
+    }
+  }
 }
 
 start(3)
-help()
-name = readline("what is your name: ")
-
+terminal()
 
 
